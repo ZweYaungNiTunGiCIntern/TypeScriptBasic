4ae2c04242674692e129388f086fddfa360a6d34
ZweYaungNiTunGicIntern <zweyaungnitun@gicjp.com>
2024-03-04 14:01:32 +0630

    Array and Object Literals

---
+++ hello.js
@@ -0,0 +1,12 @@
+"use strict";
+let indexes = [2, 3, 4, 5];
+indexes.push(45);
+console.log(indexes);
+let namae = ['Harry', 'David', 'Robin'];
+namae.push('Zwe');
+console.log(namae);
+let apple = {
+    name: 'Zwe',
+    age: 21
+};
+console.log(apple.name);
---
+++ hello.ts
@@ -0,0 +1,24 @@
+
+let indexe = [2,3,4,5]
+indexe.push(45)
+console.log(indexe)
+let namae1=['Harry','David','Robin']
+namae1.push('Zwe')
+console.log(namae1)
+let apple1={
+    name: 'Zwe',
+    age: 21
+}
+console.log(apple1.name)
+
+let dog=['lewei']
+
+let user: {firstName: string, lastName: string, points: number}={
+    firstName: 'Kate',
+    lastName: 'Harrison',
+    points: 45
+}
+user.firstName = 'Harry'
+user.lastName = 'Calvin'
+user.points = 75
+console.log(user)
